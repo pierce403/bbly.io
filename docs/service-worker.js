4ef6b8a5e2e17136b00c8bba8f95d37b04506832
Dean Pierce <pierce403@gmail.com>
2019-01-17 03:29:58 +0000

    deploy!

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19c6c39c48aa16b80fdee72bc1664dfa.js"
+  "/precache-manifest.51b9d407c548515bc2c365a71b88f1ca.js"
 );
 
 workbox.clientsClaim();
